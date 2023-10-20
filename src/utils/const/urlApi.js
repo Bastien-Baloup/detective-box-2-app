890a3770bcffe77c3b21d1e35bee09701f9ff7a8
Badbmo <82469743+badbmo@users.noreply.github.com>
2023-10-20 21:44:11 +0200

    boilerplate for API

---
+++ src/utils/const/urlApi.js
@@ -0,0 +1,8 @@
+export const urlApi = {
+	// userMainData(userId) {
+	// 	return `http://localhost:3000/user/${userId}`;
+	// },
+	apiRemi() {
+		return "https://api.detectivebox.remimichel.fr/documents?name=";
+	},
+};
