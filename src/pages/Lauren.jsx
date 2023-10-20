9cf917c1d97aa7837b6e0a7024a20ff2c8c5d5be
Badbmo <82469743+badbmo@users.noreply.github.com>
2023-10-20 17:44:01 +0200

    added sound on agent page

--- src/pages/Lauren.jsx
+++ src/pages/Lauren.jsx
@@ -3,37 +3,55 @@ import Input from "../components/Input.jsx";
 import Cross from "../assets/icons/Icon_Cross-white.svg";
 import PropTypes from "prop-types";
 
-const Lauren = ({ value, setValue, closeAgentPage }) => {
+const Lauren = ({ value, setValue, closeAgentPage, url }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log("ça fait quelque chose");
 	};
 
+	const catchphrase = [
+		"sounds/402-repliques-lauren-1.wav",
+		"sounds/402-repliques-lauren-2.wav",
+		"sounds/402-repliques-lauren-3.wav",
+		"sounds/402-repliques-lauren-4.wav",
+		"sounds/402-repliques-lauren-5.wav",
+		"sounds/402-repliques-lauren-6.wav",
+		"sounds/402-repliques-lauren-7.wav",
+	];
+
+	const randomNumber = Math.floor(Math.random() * catchphrase.length);
+
 	return (
-		<div className="agent">
-			<div className="agent__portrait--container">
-				<img className="agent__portrait" src={PhotoLauren} />
-			</div>
-			<div className="agent__main">
-				<div className="agent__title--container">
-					<p className="agent__title">Qui souhaitez vous interroger ?</p>
+		<>
+			<audio autoPlay>
+				<source src={url + catchphrase[randomNumber]} type="audio/wav" />
+				Votre navigateur ne prend pas en charge ce format
+			</audio>
+			<div className="agent">
+				<div className="agent__portrait--container">
+					<img className="agent__portrait" src={PhotoLauren} />
 				</div>
-				<form className="agent__form" onSubmit={handleSubmit}>
-					<Input
-						type="texte"
-						label="Nom et prénom"
-						name="lauren"
-						placeholder="Ce champ est vide"
-						value={value}
-						setValue={setValue}
-					/>
-					<button className="agent__form__button button--red">Valider</button>
-				</form>
+				<div className="agent__main">
+					<div className="agent__title--container">
+						<p className="agent__title">Qui souhaitez vous interroger ?</p>
+					</div>
+					<form className="agent__form" onSubmit={handleSubmit}>
+						<Input
+							type="texte"
+							label="Nom et prénom"
+							name="lauren"
+							placeholder="Ce champ est vide"
+							value={value}
+							setValue={setValue}
+						/>
+						<button className="agent__form__button button--red">Valider</button>
+					</form>
+				</div>
+				<button className="agent__closeButton--container" onClick={closeAgentPage}>
+					<img src={Cross} className="agent__closeButton" />
+				</button>
 			</div>
-			<div className="agent__closeButton--container" onClick={closeAgentPage}>
-				<img src={Cross} className="agent__closeButton" />
-			</div>
-		</div>
+		</>
 	);
 };
 
--- src/pages/Tim.jsx
+++ src/pages/Tim.jsx
@@ -3,37 +3,55 @@ import Input from "../components/Input.jsx";
 import Cross from "../assets/icons/Icon_Cross-white.svg";
 import PropTypes from "prop-types";
 
-const Tim = ({ value, setValue, closeAgentPage }) => {
+const Tim = ({ value, setValue, closeAgentPage, url }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log("ça fait quelque chose");
 	};
 
+	const catchphrase = [
+		"sounds/404-repliques-tim-1.wav",
+		"sounds/404-repliques-tim-2.wav",
+		"sounds/404-repliques-tim-3.wav",
+		"sounds/404-repliques-tim-4.wav",
+		"sounds/404-repliques-tim-5.wav",
+		"sounds/404-repliques-tim-6.wav",
+		"sounds/404-repliques-tim-7.wav",
+	];
+
+	const randomNumber = Math.floor(Math.random() * catchphrase.length);
+
 	return (
-		<div className="agent">
-			<div className="agent__portrait--container">
-				<img className="agent__portrait" src={PhotoTim} />
-			</div>
-			<div className="agent__main">
-				<div className="agent__title--container">
-					<p className="agent__title">Que souhaitez vous analyser ?</p>
+		<>
+			<audio autoPlay>
+				<source src={url + catchphrase[randomNumber]} type="audio/wav" />
+				Votre navigateur ne prend pas en charge ce format
+			</audio>
+			<div className="agent">
+				<div className="agent__portrait--container">
+					<img className="agent__portrait" src={PhotoTim} />
 				</div>
-				<form className="agent__form" onSubmit={handleSubmit}>
-					<Input
-						type="texte"
-						label="Element à analyser"
-						name="tim"
-						placeholder="Ce champ est vide"
-						value={value}
-						setValue={setValue}
-					/>
-					<button className="agent__form__button button--red">Valider</button>
-				</form>
+				<div className="agent__main">
+					<div className="agent__title--container">
+						<p className="agent__title">Que souhaitez vous analyser ?</p>
+					</div>
+					<form className="agent__form" onSubmit={handleSubmit}>
+						<Input
+							type="texte"
+							label="Element à analyser"
+							name="tim"
+							placeholder="Ce champ est vide"
+							value={value}
+							setValue={setValue}
+						/>
+						<button className="agent__form__button button--red">Valider</button>
+					</form>
+				</div>
+				<div className="agent__closeButton--container" onClick={closeAgentPage}>
+					<img src={Cross} className="agent__closeButton" />
+				</div>
 			</div>
-			<div className="agent__closeButton--container" onClick={closeAgentPage}>
-				<img src={Cross} className="agent__closeButton" />
-			</div>
-		</div>
+		</>
 	);
 };
 
