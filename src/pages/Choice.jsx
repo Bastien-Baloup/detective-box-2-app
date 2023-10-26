6985da52124948b3c7b80d61709739211fae06a4
Badbmo <82469743+badbmo@users.noreply.github.com>
2023-10-26 21:13:37 +0200

    added a reset button

--- src/pages/Choice.jsx
+++ src/pages/Choice.jsx
@@ -6,13 +6,16 @@ import Saison1 from "../assets/img/Facing-episode1.jpg";
 import Saison2 from "../assets/img/Facing-episode2.jpg";
 import Saison3 from "../assets/img/Facing-episode3.jpg";
 import { AuthContext } from "../utils/context/fetchContext.jsx";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 // ICI METTRE UN CALL API POUR RECUPERER STATE DES BOXS ? ou juste avec url ? //
+// ICI METTRE UN CALL API POUR RESET TOUT LES STATES DE TOUTES LES CONST EN FALSE //
 
 function Choice() {
 	const { loggedIn } = useContext(AuthContext);
+	const [modalReset1, setModalReset1] = useState(false);
+	const [modalReset2, setModalReset2] = useState(false);
 
 	if (!loggedIn) {
 		return <Navigate to="/sign-in" />;
@@ -46,12 +49,67 @@ function Choice() {
 		);
 	};
 
+	const nextStepReset = () => {
+		setModalReset2(true);
+		setModalReset1(false);
+	};
+
+	const displayModalReset1 = () => {
+		return (
+			<div className="modal-boxdone__background">
+				<div className="modal-boxdone__box">
+					<p className="modal-boxdone__text">
+						Voulez-vous recommencer l&apos;aventure depuis le début ? <br></br>
+						Attention, cette action est irreversible et supprimera votre avancement actuel dans l&apos;enquête !
+					</p>
+					<button className="modal-boxdone__button button--red" onClick={nextStepReset}>
+						Oui, supprimez mon avancement
+					</button>
+					<button className="modal-boxdone__button button--red" onClick={() => setModalReset1(false)}>
+						Non, retour à l&apos;enquête
+					</button>
+				</div>
+			</div>
+		);
+	};
+
+	const displayModalReset2 = () => {
+		return (
+			<div className="modal-boxdone__background">
+				<div className="modal-boxdone__box">
+					<p className="modal-boxdone__text">
+						Merci de confirmer la remise à zéro de votre avancement dans l&apos;enquête <br></br>
+						Vous comprenez que cette action est irreversible et que nous ne serons pas en mesure de retrouver votre
+						sauvegarde.
+					</p>
+					<button className="modal-boxdone__button button--red" onClick={resetGame}>
+						Oui, supprimez !
+					</button>
+					<button className="modal-boxdone__button button--red" onClick={() => setModalReset2(false)}>
+						Non, retour à l&apos;enquête
+					</button>
+				</div>
+			</div>
+		);
+	};
+
+	const resetGame = () => {
+		alert("Mock : Vous avez supprimé votre partie");
+		setModalReset2(false);
+		// ICI METTRE UN CALL API POUR RESET TOUT LES STATES DE TOUTES LES CONST EN FALSE //
+	};
+
 	return (
 		<main className="choice">
 			<img className="choice__logo" src={Logo} />
 			<h1 className="choice__title">Bienvenue Agents</h1>
 			<p className="choice__subtitle">Veuillez sélectionner votre niveau d&apos;avancement</p>
 			{displayBoxChoice()}
+			<button className="choice__reset button--red" onClick={() => setModalReset1(true)}>
+				Recommencer l&apos;aventure
+			</button>
+			{modalReset1 ? displayModalReset1() : ""}
+			{modalReset2 ? displayModalReset2() : ""}
 		</main>
 	);
 }
