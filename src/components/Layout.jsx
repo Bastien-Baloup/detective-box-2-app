63d77b26598c642fc608421b3f0bcc2951d4655b
Badbmo <82469743+badbmo@users.noreply.github.com>
2023-10-27 02:49:16 +0200

    moved check current box to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import { BoxContext } from "../utils/context/fetchContext.jsx";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
 const Layout = () => {
-	return (
-		<>
-			<Header />
-			<Outlet />
-			<Footer />
-		</>
-	);
+	const { currentBox } = useContext(BoxContext);
+	if (currentBox == null) {
+		return <Navigate to="/box-choice" />;
+	} else {
+		return (
+			<>
+				<Header />
+				<Outlet />
+				<Footer />
+			</>
+		);
+	}
 };
 
 export default Layout;
