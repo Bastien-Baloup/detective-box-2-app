3378c90c045385d0c11716f4c79f5585bf2fa66c
Badbmo <82469743+badbmo@users.noreply.github.com>
2023-11-01 16:26:12 +0100

    boilerplate dataEvent

---
+++ src/utils/const/dataEvent.js
@@ -0,0 +1,53 @@
+export const dataEvent = {
+	box1: [
+		{
+			id: 1,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+		{
+			id: 2,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+		{
+			id: 3,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+	],
+	box2: [
+		{
+			id: 1,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+		{
+			id: 2,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+		{
+			id: 3,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+	],
+	box3: [
+		{
+			id: 1,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+		{
+			id: 2,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+		{
+			id: 3,
+			// description : "Cet event fait ça : ",
+			status: "closed",
+		},
+	],
+};
